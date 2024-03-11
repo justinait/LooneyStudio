e500b463ed618a280110dc97807e75d1dd22f8f0
justinait <justiiturraspe@gmail.com>
2024-03-11 14:45:19 -0300

    footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import './Footer.css'
+import ig from '/instagram.png'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footerContainer'>
+      <p to='/' className='footerLooney'>THE LOONEY STUDIO</p>
+
+      <img src={ig} className='instagramIconFooter' alt="instagram" />
+      
+      <p className='emailFooter'>hello@thelooneystudio.com</p>
+    </div>
   )
 }
 
