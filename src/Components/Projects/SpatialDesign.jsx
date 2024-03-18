0109d8cadcc1621a757187063abd552629d7a08e
justinait <justiiturraspe@gmail.com>
2024-03-17 21:32:27 -0300

    spatial design

--- src/Components/Projects/SpatialDesign.jsx
+++ src/Components/Projects/SpatialDesign.jsx
@@ -4,6 +4,14 @@ import { Link } from 'react-router-dom'
 import spray from '/sprays/spatialSpray.png'
 
 function SpatialDesign() {
+  const spatial = [
+    { name: 'BENEFIT', image: '/spatial/Benefit.png', className: '' },
+    { name: 'HUDA BEAUTY', image: '/spatial/Huda.jpeg', className: ''},
+    { name: 'PINK PANTHERESS', image: '/spatial/Gisou.jpg', className: ''},
+    { name: 'GISOU', image: '/spatial/Gisou.jpg', className: ''},
+    { name: 'WWE', image: '/spatial/WWE.jpg', className: ''},
+    { name: 'DESTREE', image: '/spatial/Destree.jpg', className: ''},
+  ]
   return (
     <div>
       <div className='headerProjects'>
@@ -13,6 +21,20 @@ function SpatialDesign() {
         <img src={spray} alt="" className='spraySpatial' />
         
         <Link to={'/tvAndFilms'} className='linksSpatial linksProjects projectsRigthLink'>TV & FILMS</Link>
+      </div>
+
+      <div className='spatialImagesContainer'>
+        {
+        spatial.map(e=>{
+          return(
+            <div className='spatialItemBox'>
+              <img src={e.image} alt={e.name} className='spatialImage'/>
+              <div className='imageOverlay'></div>
+              <p className='spatialText'>{e.name}</p>
+            </div>
+          )
+        })
+        }
       </div>
     </div>
   )
