c127a4ee86cbc3cc6f6e4d7b6c6752cb14aa1137
justinait <justiiturraspe@gmail.com>
2024-04-24 16:40:49 -0300

    navbar options

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Header.css'
 import { Link, useNavigate } from 'react-router-dom';
 import MenuIcon from '/menuIcon.png'
@@ -6,12 +6,33 @@ import CloseIcon from '/closeIcon.png'
 import arrow from '/arrow.png'
 import arrowback from '/arrowback.png'
 import ig from '/instagram.png'
+import spray from '/sprays/aboutSpray.png'
+import spray2 from '/sprays/AssetsHero.png'
 
 function Header() {
   const [openMenu, setOpenMenu] = useState(false);
   const [openNextMenu, setOpenNextMenu] = useState(false);
   const [selectedCategory, setSelectedCategory] = useState('home')
+  const [prevScrollPos, setPrevScrollPos] = useState(window.pageYOffset);
+  const [visible, setVisible] = useState(true);
+  
   const navigate = useNavigate();
+  
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.scrollY;
+      const isVisible = prevScrollPos > currentScrollPos;
+
+      setPrevScrollPos(currentScrollPos);
+      setVisible(isVisible);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [prevScrollPos]);
 
   const handleOpen = () => {
     if(openMenu)
@@ -50,7 +71,7 @@ function Header() {
       <div className='initialHeader'>
         <Link to='/' className='navbarLooney'>THE LOONEY STUDIO</Link>
         {openMenu ?
-        <img src={CloseIcon}  onClick={handleOpen} className='closeMenuIcon' alt="Menu" />:
+        <img src={CloseIcon}  onClick={handleClose} className='closeMenuIcon' alt="Menu" />:
         <img src={MenuIcon}  onClick={handleOpen} className='burgerMenuIcon' alt="Menu" />
         
         }
@@ -74,16 +95,17 @@ function Header() {
             })}
             
           </div>
-          
+          <img src={spray} alt="" className='sprayDropdown1' />
           <img src={ig} alt="Instagram" className='igIconNavbar'/>
         </div>
       }
       {openNextMenu &&
-        <div className='dropdownHeader'>
+        <div className='dropdownHeader nextDropdown'>
           
           <div className='dropdownItemsContainer'>
             <p  onClick={()=>handlePrevMenu()} className={selectedCategory == 'projects'? 'activeNavbar projects': 'projects'}> <img src={arrowback} alt="<" />            </p>
           
+            <img src={spray2} alt="" className='sprayDropdown2' />
             {nextSections.map((e, i)=> {
               return (
               <Link 
@@ -93,6 +115,8 @@ function Header() {
                 {e.name}
               </Link>)
             })}
+            
+          <img src={ig} alt="Instagram" className='igIconNavbar'/>
             
           </div>
         </div>
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import hero from '/heroaux.png'
-import sprayHero from '/sprays/AssetsHero.png'
 import './Home.css'
 
 function Home() {
   return (
     <div className='heroContainer'>
-      <img src={hero} alt="Looney" className='heroImage' />
-      {/* <p>DESIGN & BUILD</p>
-      <img src={sprayHero} alt="" /> */}
+      <video 
+        className='heroImage' 
+        src="https://s3-figma-videos-production-sig.figma.com/video/1336690570407106185/TEAM/523e/bd42/-db2b-4da4-a010-312139035f00?Expires=1714953600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=pfCivlAHioOeAevC2Z0sT3DStKydwKtb7Rend2DRSRRY9Nz4MHVOxMu2tskwNd61vAlQXaAKTPlm3ny4FXDzu9vGt0Trp2oMfDXjDZCpo8I5BBEBZX99NBlmq-Ybb2VYF169LBpcjc7k96NXKTKAo2RDz8H~iQuOpXivkwVoyEkyQNccavbUCHBUgWrkOoV58AtR93ghQcAsjC7XEiGX487Yzz1uX62AnPQ2eEGoeV3NsYQoRVUptIu0ETVDNO7hg3FD-nRePuaZop3VgSQw9DM~oY2ZDbRdmgT-t3D8DRWkNrD7hZKG3sW-1l~x5di2vUulXaAxo08KWTY6sRdE5Q__" 
+        muted loop autoplay ></video>
+      <p className='heroText'>DESIGN & BUILD</p>
+      
     </div>
   )
 }
