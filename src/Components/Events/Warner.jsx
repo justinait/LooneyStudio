ea758608f40af90eb91563cba4c4441e106949c7
justinait <justiiturraspe@gmail.com>
2024-04-27 22:37:30 -0300

    warner 23, plantilla orange

--- src/Components/Events/Warner.jsx
+++ src/Components/Events/Warner.jsx
@@ -22,85 +22,87 @@ function Warner() {
   return (
     <div>
       <img src={xSpray} className='xSprayEvent' alt="" />
-      <Link to={'/events'}><p className='xSpatialDetail'>X</p></Link>
+      <Link to={'/events'}><p className='xEventDetail'>X</p></Link>
       
       <img src={hero} alt="BENEFIT" className='blancHero'/>
-      <div className='stackContainer stackContainerBenefit'>
-        <div className='stackItem2'>
+      <div className='stackContainerEvents'>
+        <div className='stackItemEventsLarger'>
           <img className='stackCircleImg' src={circle1} alt="" />
           <p className='stackText'>CONCEPT DESIGN</p>
         </div>
-        <div className='stackItemPink1'>
+        <div className='stackItemEventsLarge'>
           <img className='stackCircleImg' src={circle2} alt="" />
           <p className='stackText'>PROJECT MANAGEMENT</p>
         </div>
-        <div className='stackItemPink3'>
+        <div className='stackItemEvents'>
           <img className='stackCircleImg' src={circle3} alt="" />
+          <p className='stackText'>INSTALLATION</p>
+        </div>
+        <div className='stackItemEvents9'>
+          <img className='stackCircleImg' src={circle4} alt="" />
+          <p className='stackText'>EVENT</p>
+        </div>
+        <div className='stackItemEvents'>
+          <img className='stackCircleImg' src={circle5} alt="" />
           <p className='stackText'>PROP MAKING</p>
         </div>
 
-        <div className='stackItem4'>
-          <img className='stackCircleImg' src={circle4} alt="" />
-          <p className='stackText'>INSTALLATION</p>
-        </div>
-        <div className='stackItem1'>
-          <img className='stackCircleImg' src={circle5} alt="" />
-          <p className='stackText'>LIGHTING DESIGN</p>
-        </div>
-        <div className='stackItem4'>
-          <img className='stackCircleImg' src={circle5} alt="" />
-          <p className='stackText'>CARPENTRY</p>
-        </div>
           
       </div>
-      <div className='colorSpace'>
-        <p className='titleSpatialDetail'>BENEFIT - Pore Wash Pop Up</p>
-        <div className='separatorSpatial'></div>
+      <div className='colorSpaceEvents'>
+        <p className='titleSpatialDetail'>WARNER MUSIC CHRISTMAS PARTY 23</p>
+        <div className='separatorEvents'></div>
         <div className='infoContainerSpatialDetail'>
           <div>
             <p className='infoTitleSpatialDetail'>CLIENT</p>
-            <p className='infoDataSpatialDetail'>Benefit Cosmetics</p>
+            <p className='infoDataSpatialDetail'>Warner Music</p>
           </div>
 
           <div>
             <p className='infoTitleSpatialDetail'>AGENCY</p>
-            <p className='infoDataSpatialDetail'>POP UP MOB</p>
+            <p className='infoDataSpatialDetail'>The Standard Hotel</p>
           </div>
 
-          <div>
-            <p className='infoTitleSpatialDetail'>LOCATION</p>
-            <p className='infoDataSpatialDetail'>Covent Garden</p>
-          </div>
         </div>
         <div className='infoSpatialDetailTextContainer'>
-          <p>Building upon Benefit’s successful pop-up in New York, 
-            which employed the same theme, we had just a month to 
-            meticulously craft all the necessary elements and a mere 
-            five days to bring the installation to life on site. 
-            With ingenuity and creativity at the forefront, we 
-            fashioned a fully functional car wash setup complete 
-            with spinning brushes, 
+          <p>Inspired by the 70s essence of Decimo restaurant itself, we embarked on a
+            creative endeavor to elevate the atmosphere of the venue for the Warner
+            Music end-of-year party into an
+            unforgeable experience. Spanning the
+            entire space, a captivating ceiling
+            installation stretched across the venue,
+            seing the stage for an evening of
+            unparalleled excitement.
+            A striking 50-foot suspended
+            installation guided guests into the heart
+            of the dance 
           </p>
           <p>
-          mesmerizing floating bubbles, and fuel pumps that playfully 
-          dispensed foam. The result was an interactive and experiential 
-          journey that left a lasting impression on all who engaged with it.
+            floor, where our bespoke DJ booth awaited, graced by acclaimed
+            artists like Mike Skinner from The
+            Streets.
+            Dominating the scene, a colossal floating W logo
+            served as a beacon of aention, adding a touch of
+            grandeur to the ambiance. Stealing the spotlight, a
+            magnificent 6-foot robot, meticulously crafted from
+            fiberglass, took center stage, captivating guests with its extraterrestrial allure, ensuring a night to
+            remember.
           </p>
         </div>
       </div>
       <div>
         <div className='spatialDetailImagesBoxBenefit1'>
-          <img src={image1} alt="benefit" className='' />
           <img src={image2} alt="benefit" className='' />
           <img src={image3} alt="benefit" className='' />
+          <img src={image4} alt="benefit" className='' />
         </div>
 
-        <img src={image4} className="spatialDetailImageBenefit" />
+        <img src={image5} className="spatialDetailImageWarner" />
 
         <div className='spatialDetailImagesBoxBenefit1'>
-          <img src={image5} alt="benefit" className='' />
           <img src={image6} alt="benefit" className='' />
           <img src={image7} alt="benefit" className='' />
+          <img src={image8} alt="benefit" className='' />
         </div>
 
         <p className='spatialDetailImagesText'>Behind the scenes</p>
