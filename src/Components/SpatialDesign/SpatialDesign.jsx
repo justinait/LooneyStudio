c732d935e6f28afae6659b1f55844bad0ea8c2e7
justinait <justiiturraspe@gmail.com>
2024-05-02 13:20:51 -0300

    events and spatial homes tablet, finished

--- src/Components/SpatialDesign/SpatialDesign.jsx
+++ src/Components/SpatialDesign/SpatialDesign.jsx
@@ -10,7 +10,7 @@ function SpatialDesign() {
     { name: 'PINK PANTHERESS - Heaven Knos Album', image: '/spatial/pink.png', detail: '/pink', className: 'breakSpatialText'},
     { name: 'BENEFIT - PORE WASH POP UP', image: '/spatial/Benefit.png', detail: '/benefit', className: '' },
     { name: 'GISOU - HOLIDAY POP UP', image: '/spatial/Gisou.jpg', detail: '/gisou', className: ''},
-    { name: 'HUDA BEAUTY - MERCURY RETROGADE', image: '/spatial/Huda.jpeg', detail: '/huda', className: 'breakSpatialText'},
+    { name: 'HUDA BEAUTY - MERCURY RETROGADE', image: '/spatial/Huda.jpeg', detail: '/huda', className: 'breakSpatialText breakSpatialTextUnset'},
   ]
 
   return (
