b5cdc0d785205946422693852ff37b6809eb9b7e
justinait <justiiturraspe@gmail.com>
2024-05-20 11:15:35 -0300

    map

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -12,10 +12,154 @@ function Contact() {
     };
     
     const { isLoaded } = useJsApiLoader({
-        id: '436bcb265c6ff251',
-        googleMapsApiKey: 'AIzaSyATA9w96X3FKj_idxFdo9nvd4SxaxDb43Y'
+        id: 'dc395694d3c4c44',
+        googleMapsApiKey: import.meta.env.VITE_API_MAPSKEY
     })
-
+    const mapStyles = [
+        {
+            "elementType": "geometry",
+            "stylers": [
+                { "color": "#212121" }
+            ]
+        },
+        {
+            "elementType": "labels.icon",
+            "stylers": [
+                { "visibility": "off" }
+            ]
+        },
+        {
+            "elementType": "labels.text.fill",
+            "stylers": [
+                { "color": "#757575" }
+            ]
+        },
+        {
+            "elementType": "labels.text.stroke",
+            "stylers": [
+                { "color": "#212121" }
+            ]
+        },
+        {
+            "featureType": "administrative",
+            "elementType": "geometry",
+            "stylers": [
+                { "color": "#757575" }
+            ]
+        },
+        {
+            "featureType": "administrative.country",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                { "color": "#9e9e9e" }
+            ]
+        },
+        {
+            "featureType": "administrative.land_parcel",
+            "stylers": [
+                { "visibility": "off" }
+            ]
+        },
+        {
+            "featureType": "administrative.locality",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                { "color": "#bdbdbd" }
+            ]
+        },
+        {
+            "featureType": "poi",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                { "color": "#757575" }
+            ]
+        },
+        {
+            "featureType": "poi.park",
+            "elementType": "geometry",
+            "stylers": [
+                { "color": "#181818" }
+            ]
+        },
+        {
+            "featureType": "poi.park",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                { "color": "#616161" }
+            ]
+        },
+        {
+            "featureType": "poi.park",
+            "elementType": "labels.text.stroke",
+            "stylers": [
+                { "color": "#1b1b1b" }
+            ]
+        },
+        {
+            "featureType": "road",
+            "elementType": "geometry.fill",
+            "stylers": [
+                { "color": "#2c2c2c" }
+            ]
+        },
+        {
+            "featureType": "road",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                { "color": "#8a8a8a" }
+            ]
+        },
+        {
+            "featureType": "road.arterial",
+            "elementType": "geometry",
+            "stylers": [
+                { "color": "#373737" }
+            ]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "geometry",
+            "stylers": [
+                { "color": "#3c3c3c" }
+            ]
+        },
+        {
+            "featureType": "road.highway.controlled_access",
+            "elementType": "geometry",
+            "stylers": [
+                { "color": "#4e4e4e" }
+            ]
+        },
+        {
+            "featureType": "road.local",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                { "color": "#616161" }
+            ]
+        },
+        {
+            "featureType": "transit",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                { "color": "#757575" }
+            ]
+        },
+        {
+            "featureType": "water",
+            "elementType": "geometry",
+            "stylers": [
+                { "color": "#000000" }
+            ]
+        },
+        {
+            "featureType": "water",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                { "color": "#3d3d3d" }
+            ]
+        }
+    ];
+    
   return (
     <div className='contactContainer'>
         <div className='contactUsContainer'>
@@ -32,22 +176,25 @@ function Contact() {
             <div className='containerBox mobileOnly'>
                 {/* empty */}
             </div>
+
             <div className='containerBox biggerBoxAbout'>
                 <div className='mapBoxContainer'>
                     {isLoaded && 
                         <GoogleMap 
-                        zoom={12}
-                        center={{ lat: 51.466214348028984, lng: -0.10306554686605598 }}
-                        mapContainerClassName='mapContainer'
-                        mapId='436bcb265c6ff251'
-                        className='mapContainer'
+                            zoom={12}
+                            center={{ lat: 51.466214348028984, lng: -0.10306554686605598 }}
+                            mapContainerClassName='mapContainer'
+                            mapId='dc395694d3c4c44'
+                            className='mapContainer'
+                            options={{ styles: mapStyles }}
                         >
                         <Marker 
-                        position={{ lat: 51.466214348028984, lng: -0.10306554686605598 }} 
+                            position={{ lat: 51.466214348028984, lng: -0.10306554686605598 }} 
                         />
                         </GoogleMap>
                     }
                 </div>
+
                 <div className='instagramBox'>
                     <h3>INSTAGRAM</h3>
                     <p className='locationText'>@Thelooneystudio</p>
@@ -66,7 +213,7 @@ function Contact() {
             </div>
         </div>
         
-        <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyATA9w96X3FKj_idxFdo9nvd4SxaxDb43Y&callback=initMap&map_ids=436bcb265c6ff251"></script>
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyATA9w96X3FKj_idxFdo9nvd4SxaxDb43Y&callback=initMap&map_ids=dc395694d3c4c44" defer></script>
         
     </div>
   )
