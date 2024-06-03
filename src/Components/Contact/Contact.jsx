19e024cee3b6cb48267298cb9177321b2b43d8e2
justinait <justiiturraspe@gmail.com>
2024-06-03 11:56:30 -0300

    sprays contact services about - mobile

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -180,57 +180,60 @@ function Contact() {
     }, [isLoaded]);
 
   return (
-    <div className='contactContainer'>
-        <div className='contactUsContainer'>
-            <p className='contactUs'>Contact <br /> Us</p>
+    <div className='contactSprayContainer'>
+
+        <div className='contactContainer'>
+            <div className='contactUsContainer'>
+                <p className='contactUs'>Contact <br /> Us</p>
+            </div>
+            <div className='contactContainerBox'>
+
+                <div className='containerBox halfDesktop'>
+                    <h3>LOCATION</h3>
+                    <p className='locationText'>
+                    Unit 2-5, 496 Rathgar Road<br className='breakMobileAndTablet' /> SW9 7EP <strong>London</strong> 
+                    </p>
+                </div>
+                <div className='containerBox mobileOnly'>
+                    {/* empty */}
+                </div>
+
+                <div className='containerBox biggerBoxAbout'>
+                    <div className='mapBoxContainer'>
+                        {isLoaded && 
+                            <GoogleMap 
+                                zoom={12}
+                                center={{ lat: 51.466214348028984, lng: -0.10306554686605598 }}
+                                mapContainerClassName='mapContainer'
+                                options={{ styles: mapStyles }}
+                                ref={mapRef}
+                            >
+                            <Marker position={markerPosition}/>
+                            </GoogleMap>
+                        }
+                    </div>
+
+                    <div className='instagramBox'>
+                        <h3>INSTAGRAM</h3>
+                        <p className='locationText'>@Thelooneystudio</p>
+                    </div>
+                </div>
+
+                <div className='containerBox halfBigDesktop'>
+                    <h3>MAIL</h3>
+                    <div>
+                        <input type="text" placeholder='Full Name' className={`inputContact locationText ${inputFilled ? 'color-activo' : ''}`} onChange={handleInputChange}/>
+                        <input type="email" placeholder='Mail' className={`inputContact locationText ${inputFilled ? 'color-activo' : ''}`} onChange={handleInputChange}/>
+                        <input type="text" placeholder='Your Message' className={`inputContact locationText ${inputFilled ? 'color-activo' : ''}`} onChange={handleInputChange}/>
+                    </div>
+                    <p className='formButton'>Send</p>
+                    <img src={spray} id='spraySend' alt="" />
+                </div>
+            </div>
+            
+            {/* <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyATA9w96X3FKj_idxFdo9nvd4SxaxDb43Y&callback=initMap&map_ids=dc395694d3c4c44" defer></script> */}
+            
         </div>
-        <div className='contactContainerBox'>
-
-            <div className='containerBox halfDesktop'>
-                <h3>LOCATION</h3>
-                <p className='locationText'>
-                Unit 2-5, 496 Rathgar Road<br className='breakMobileAndTablet' /> SW9 7EP <strong>London</strong> 
-                </p>
-            </div>
-            <div className='containerBox mobileOnly'>
-                {/* empty */}
-            </div>
-
-            <div className='containerBox biggerBoxAbout'>
-                <div className='mapBoxContainer'>
-                    {isLoaded && 
-                        <GoogleMap 
-                            zoom={12}
-                            center={{ lat: 51.466214348028984, lng: -0.10306554686605598 }}
-                            mapContainerClassName='mapContainer'
-                            options={{ styles: mapStyles }}
-                            ref={mapRef}
-                        >
-                        <Marker position={markerPosition}/>
-                        </GoogleMap>
-                    }
-                </div>
-
-                <div className='instagramBox'>
-                    <h3>INSTAGRAM</h3>
-                    <p className='locationText'>@Thelooneystudio</p>
-                </div>
-            </div>
-
-            <div className='containerBox halfBigDesktop'>
-                <h3>MAIL</h3>
-                <div>
-                    <input type="text" placeholder='Full Name' className={`inputContact locationText ${inputFilled ? 'color-activo' : ''}`} onChange={handleInputChange}/>
-                    <input type="email" placeholder='Mail' className={`inputContact locationText ${inputFilled ? 'color-activo' : ''}`} onChange={handleInputChange}/>
-                    <input type="text" placeholder='Your Message' className={`inputContact locationText ${inputFilled ? 'color-activo' : ''}`} onChange={handleInputChange}/>
-                </div>
-                <p className='formButton'>Send</p>
-                <img src={spray} id='spraySend' alt="" />
-            </div>
-        </div>
-        
-        {/* <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyATA9w96X3FKj_idxFdo9nvd4SxaxDb43Y&callback=initMap&map_ids=dc395694d3c4c44" defer></script> */}
-        
     </div>
   )
 }
