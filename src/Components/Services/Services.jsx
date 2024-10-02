5a30e67d826b78921db71b5b7839302850b9bf35
justinait <justiiturraspe@gmail.com>
2024-10-02 17:41:00 -0300

    services images folder

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,14 +1,20 @@
 import React, { useEffect, useRef, useState } from 'react'
 import './Services.css'
-import spray from '/sprays/tvSpray.png'
+import spray from '../../assets/sprays/tvSpray.png'
+import conceptdesign from '../../assets/services/conceptdesign.png'
+import projectmanagement from '../../assets/services/projectmanagement.png'
+import carpentry from '../../assets/services/carpentry.png'
+import metalwork from '../../assets/services/metalwork.png'
+import cncing from '../../assets/services/cncing.png'
+import spraying from '../../assets/services/spraying.png'
+import installation from '../../assets/services/installation.png'
+import hire from '../../assets/services/hire.png'
 
 function Services() {
-
     const form = useRef();
     const service_id = import.meta.env.VITE_API_SERVICEID;
     const template_id = import.meta.env.VITE_API_TEMPLATEID;
     const public_id = import.meta.env.VITE_API_PUBLICID;
-  
     const [open, setOpen] = useState(false);
     const [openServicesForm, setOpenServicesForm] = useState(false);
     const [service, setService] = useState('');
@@ -24,16 +30,15 @@ function Services() {
     });
     const [formSent, setFormSent] = useState(false);
     const [showErrors, setShowErrors] = useState(false);
-
     const services = [
-        { name: 'CONCEPT DESIGN', image: '/services/conceptdesign.png', text: 'Our design team swiftly transforms ideas into reality, crafting mood boards, sketches, and 3D models that embody your vision. Emphasizing aesthetics, functionality, and meticulous attention to detail, we seamlessly merge innovation and practicality in our design process.', className: '' },
-        { name: 'PROJECT MANAGEMENT', image: '/services/projectmanagement.png', text: 'Our dedicated project managers oversee every aspect of the build, ensuring communication, progress monitoring, and budget adherence. Renowned for flexibility, they provide guidance and support throughout the process.', className: 'littleServicesText'},
-        { name: 'CARPENTRY', image: '/services/carpentry.png', text: 'Nestled in Brixton, our 3000 sqft workshop is fully equipped to handle multiple projects of any scale. Our skilled carpenters embrace collaboration and sustainability, repurposing every piece of wood to minimize waste.', className: 'littleServicesText'},
-        { name: 'METAL WORK', image: '/services/metalwork.png', text: 'Fortunately, our metal workers are conveniently located nearby, ready to fulfill all your fabrication needs. With expertise in intricate details and robust structures, we handle diverse metal projects efficiently. This proximity fosters seamless collaboration with our woodwork team, enabling innovative combinations and swift, quality deliveries.', className: 'bigServicesText biggerLetter'},
-        { name: 'CNCING', image: '/services/cncing.png', text: 'With our CNC machining capabilities, we offer rapid turnaround for intricate builds needing precise cuts. Our large format CNC machine, with a 10ft by 5ft bed size, can cut various materials, including full sheets, for the production of larger components as needed.', className: 'biggerLetter'},
-        { name: 'FINISHING & SPRAYING', image: '/services/spraying.png', text: 'From scenic painting to unique custom finishes, we excel in creating any desired texture envisioned by our designers and clients. Just as we prioritize excellent wood joinery, we also prioritize top-notch finishes. With a talented roster of trained scenic painters, sprayers, and prop makers, we consistently deliver the highest quality, bringing bold aesthetic visions to life.', className: 'bigServicesText'},
-        { name: 'INSTALLATION', image: '/services/installation.png', text: "We don't just create; we install too! Our skilled crew of carpenters, decorators, electricians, and scenics collaborate seamlessly to bring sets to life quickly. From film sets to events, we deliver unmatched speed and precision.", className: 'littleServicesText'},
-        { name: 'HIRE', image: '/services/hire.png', text: 'Need scenic equipment urgently? Our stocked inventory includes flats, doors, windows, flooring, braces, stage weights, and more, available for same-day hire. We can even manage delivery and installation if required.  <strong id="enquireHere">ENQUIRE HERE</strong>', className: 'littleServicesText'}
+        { name: 'CONCEPT DESIGN', image: conceptdesign, text: 'Our design team swiftly transforms ideas into reality, crafting mood boards, sketches, and 3D models that embody your vision. Emphasizing aesthetics, functionality, and meticulous attention to detail, we seamlessly merge innovation and practicality in our design process.', className: '' },
+        { name: 'PROJECT MANAGEMENT', image: projectmanagement, text: 'Our dedicated project managers oversee every aspect of the build, ensuring communication, progress monitoring, and budget adherence. Renowned for flexibility, they provide guidance and support throughout the process.', className: 'littleServicesText'},
+        { name: 'CARPENTRY', image: carpentry, text: 'Nestled in Brixton, our 3000 sqft workshop is fully equipped to handle multiple projects of any scale. Our skilled carpenters embrace collaboration and sustainability, repurposing every piece of wood to minimize waste.', className: 'littleServicesText'},
+        { name: 'METAL WORK', image: metalwork, text: 'Fortunately, our metal workers are conveniently located nearby, ready to fulfill all your fabrication needs. With expertise in intricate details and robust structures, we handle diverse metal projects efficiently. This proximity fosters seamless collaboration with our woodwork team, enabling innovative combinations and swift, quality deliveries.', className: 'bigServicesText biggerLetter'},
+        { name: 'CNCING', image: cncing, text: 'With our CNC machining capabilities, we offer rapid turnaround for intricate builds needing precise cuts. Our large format CNC machine, with a 10ft by 5ft bed size, can cut various materials, including full sheets, for the production of larger components as needed.', className: 'biggerLetter'},
+        { name: 'FINISHING & SPRAYING', image: spraying, text: 'From scenic painting to unique custom finishes, we excel in creating any desired texture envisioned by our designers and clients. Just as we prioritize excellent wood joinery, we also prioritize top-notch finishes. With a talented roster of trained scenic painters, sprayers, and prop makers, we consistently deliver the highest quality, bringing bold aesthetic visions to life.', className: 'bigServicesText'},
+        { name: 'INSTALLATION', image: installation, text: "We don't just create; we install too! Our skilled crew of carpenters, decorators, electricians, and scenics collaborate seamlessly to bring sets to life quickly. From film sets to events, we deliver unmatched speed and precision.", className: 'littleServicesText'},
+        { name: 'HIRE', image: hire, text: 'Need scenic equipment urgently? Our stocked inventory includes flats, doors, windows, flooring, braces, stage weights, and more, available for same-day hire. We can even manage delivery and installation if required.  <strong id="enquireHere">ENQUIRE HERE</strong>', className: 'littleServicesText'}
     ]
     
     const handleOpen = (e) => {
